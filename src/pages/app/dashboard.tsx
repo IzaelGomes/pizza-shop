6c5e41ece082874cf8b8d4cd7cb3536ff3b6def4
izael gomes <izaelgomes127@gmail.com>
2024-01-31 16:42:55 -0300

    feat: login page

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 export const Dashboard = () => {
-  return <div>dashboard</div>
+  return (
+    <div>
+      <Helmet title="Dashboard" />
+      <h1>Dashborad</h1>
+    </div>
+  )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,27 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 export const SignIn = () => {
-  return <div></div>
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="font text-2xl font-semibold tracking-tighter">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2"></div>
+            <button type="submit">Acessar portal</button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
