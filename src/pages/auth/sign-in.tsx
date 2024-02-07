3b0e5d9ef4d170eeff1b060b1b4cf390b7235c1a
izael gomes <izaelgomes127@gmail.com>
2024-02-06 22:50:06 -0300

    feat: component header and sign up page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSign(data: SignInForm) {
+    await new Promise((resolve, reject) => {
+      setTimeout(resolve, 2000)
+    })
+
+    toast.success('Enviamos um link de authenticacao')
+  }
+
   return (
     <>
       <Helmet title="Login" />
-      <div className="p-8">
+      <div className="p-8 ">
+        <Button asChild className="absolute right-8 top-8">
+          <Link to={'/sign-up'}>Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="font text-2xl font-semibold tracking-tighter">
@@ -20,12 +46,12 @@ export const SignIn = () => {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSign)}>
             <div className="space-y-2">
               <Label>Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Acessar painel
             </Button>
           </form>
