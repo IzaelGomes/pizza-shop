f058ad53b43c2a646739411885bb100da19f38f0
izael gomes <izaelgomes127@gmail.com>
2024-02-11 20:06:42 -0300

    feat: pizza chart on dashboard page

--- src/pages/app/dashboard/components/revenue-chart.tsx
+++ src/pages/app/dashboard/components/revenue-chart.tsx
@@ -1,11 +1,88 @@
-import { Card, CardHeader } from '@/components/ui/card'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 export function RevenueChart() {
+  const data = [
+    {
+      date: '10/12',
+      revenue: 1200,
+    },
+    {
+      date: '11/12',
+      revenue: 200,
+    },
+    {
+      date: '12/12',
+      revenue: 400,
+    },
+    {
+      date: '13/12',
+      revenue: 1500,
+    },
+    {
+      date: '14/12',
+      revenue: 1300,
+    },
+    {
+      date: '15/12',
+      revenue: 1700,
+    },
+    {
+      date: '16/12',
+      revenue: 700,
+    },
+  ]
+
   return (
-    <div>
-      <Card>
-        <CardHeader className="flex-row items-center justify-center pb-8"></CardHeader>
-      </Card>
-    </div>
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium"></CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <ResponsiveContainer width="100%" height={248}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" tickLine={false} dy={17} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet[500]}
+            />
+            <CartesianGrid vertical={false} className="stroke-muted" />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
   )
 }
